78fb9df169ecca821fba5ce900cac2eb48ae18e2
jurpiotr <jurpiotr@gmail.com>
2023-01-09 17:56:38 +0100

    Created new 3D model dice D12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DodecahedronComponent } from './dodecahedron/dodecahedron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DodecahedronComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dodecahedron/dodecahedron.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dodecahedron',
+  templateUrl: './dodecahedron.component.html',
+  styleUrls: ['./dodecahedron.component.scss']
+})
+export class DodecahedronComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
