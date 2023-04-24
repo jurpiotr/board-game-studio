c6fda31ebcdb3dbcce14fe163df1876dbd8aa0b1
jurpiotr <jurpiotr@gmail.com>
2023-04-24 23:02:54 +0200

    Added new display of dice values

--- src/app/dices/deltohedron/deltohedron.component.ts
+++ src/app/dices/deltohedron/deltohedron.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'deltohedron',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deltohedron.component.scss']
 })
 export class DeltohedronComponent implements OnInit {
+  @ViewChildren('side', {read: ElementRef}) div!: QueryList<ElementRef>;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.div.map(p => this.resizeFont(p.nativeElement))
+  }
+  resizeFont(p: HTMLElement) {
+    const valueSide = p.dataset['value']?.length;
+    if(valueSide! > 2) {
+      p.style.setProperty('font-size', '0.5rem');
+    }
+
+
+  }
 }
+
+
